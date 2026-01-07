64fcfd19bf548f26595653a9a1daefdfd68d0d22
Dipesh Panta <dipeshpanta431@gmail.com>
2026-01-07 20:31:04 +0545

    update backend

--- Backend/contact.php
+++ Backend/contact.php
@@ -1,4 +1,11 @@
 <?php
+
+if(isset($_POST['submit']))    
+{
+    $name = $_POST["uname"];
+    $email = $_POST["umail"];
+    $message = $_POST["umessage"];
+    
 $conn=mysqli_connect('localhost','root','') or die('connection error');
 
  $create="create database if not exists bachelor_Hub ";
@@ -9,26 +16,13 @@ $conn=mysqli_connect('localhost','root','') or die('connection error');
   $createtbl = "CREATE TABLE IF NOT EXISTS contact (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE,
+    email VARCHAR(100),
      message varchar(400)
 )";
 
 mysqli_query($conn, $createtbl) or die("Tale Creation Error");
-    if (!mysqli_query($conn, $createtbl)) {
-        die("Table creation error: " . mysqli_error($conn));
-    }
-    
-
-    if(isset($_POST['submit']))
-    {
-
-    $name = $_POST["uname"];
-    $email = $_POST["umail"];
-    $message = $_POST["umessage"];
-    $insert = "INSERT INTO contact (name, email, message) 
-        VALUES('$name', '$email', '$message')";
-
+    $insert = "INSERT INTO contact (name, email, message) VALUES('$name', '$email', '$message')";
     mysqli_query($conn,$insert) or die('insertion failed');
-    echo ' message sucessfully send';
+    header("Location:./contact_us.html");
     }
-  ?>
+?>
